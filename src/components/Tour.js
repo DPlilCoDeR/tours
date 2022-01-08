345bd9cf3f679bfd1aee07ba1b08f815fa54660a
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-08 17:40:02 +0100

    Tour component & styles

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({tour}) => {
+    const [tourState, setTourState] = useState(tour);
+    const [readMore, setReadMore] = useState(false)
+
+    return(
+        <article className='single-tour'>
+            <img src={tourState.image} alt={tourState.name}/>
+            <footer>
+                <div className='tour-info'>
+                    <h4>{tourState.name}</h4>
+                    <p className='tour-price'>{tourState.price}</p>
+                </div>
+            <p>
+            {readMore ? tourState.info : `${tourState.info.substring(0, 200)}...`}
+            <button onClick={() => setReadMore(!readMore)}>
+                {readMore ? 'show less' : '  read more'}
+            </button>
+            </p>
+                <button className='delete-btn' onClick={()=> deleteTour(tourState.id)}>Not interested</button>
+            </footer>
+        </article >
+    );
 };
 
 export default Tour;
