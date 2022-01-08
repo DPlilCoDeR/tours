ec1ed609849285d41f1ccfae947fbcb481fe752f
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-08 17:45:02 +0100

    Tours component & styles

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, deleteTour}) => {
+  return (
+      <section>
+          <div className='Title'>
+            <h2>Ours Tours</h2>
+            <div className='underline'></div>
+          </div>
+          <div>
+            {tours.map( tour => <Tour key={tour.id} tour={tour} deleteTour={deleteTour}/>)}
+          </div>
+      </section>
+  );
 };
 
 export default Tours;
